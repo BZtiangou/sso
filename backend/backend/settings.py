c7d705771b654ecc921026280014e1053ff83c78
uaih3k9x <uaih3k9x@uaih3k9x.feroda39>
2024-09-11 15:44:44 +0800

    修复字符串问题

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -105,7 +105,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if  DEBUG:
+
+if DEBUG=='True':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
